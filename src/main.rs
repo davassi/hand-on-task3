60d1fc4d1db47d13b8fe6a364e72c0741b354b0e
Gianluigi <1568018+davassi@users.noreply.github.com>
2023-09-07 17:46:04 +0200

    Second commit of hand-on-task3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 
+use std::marker::{self, PhantomData};
 
 /// F defines the function, T the type
 struct FilterCondition<F, T> 
 where
     F: Fn(T) -> bool,
+    T: Copy
 {
     condition : F,
+    _m : marker::PhantomData<T>,
 }
 
 impl<F, T: Copy> FilterCondition<F, T> 
 where
     F: Fn(T) -> bool,
 {
-
-    fn new(condition: fn(T) -> bool) -> Self {
-        FilterCondition { condition }
+    fn new(condition: F) -> Self {
+        FilterCondition { condition , _m : PhantomData }
     }
 
     fn is_match(&self, c: &T) -> bool {
@@ -27,11 +29,15 @@ fn custom_filter<F : Fn(T,) -> bool, T: Copy>(v : &Vec<T>, predicate: &FilterCon
 }
 
 fn main() -> () {
-    
-    let v : Vec<u32> = (0..20).collect();
 
-    let predicate = FilterCondition::new(|x: u32| { x<10 });
-    let v : Vec<u32> = custom_filter(&v, &predicate);
+    let predicate = FilterCondition::new(|x: i32| { x<0 });
+    let v : Vec<i32> = custom_filter(&(-10..10).collect(), &predicate);
     
-    println!("The filtered vec: {:?}",v);
+    println!("A i32 filtered vec: {:?}",v);
+
+    let chars : Vec<char> = vec!['a','A','b','B','c','C','d','D']; 
+    let predicate = FilterCondition::new(|x: char| { x.is_uppercase() });
+    let v : Vec<char> = custom_filter(&chars, &predicate);
+    
+    println!("A char filtered vec: {:?}",v);
 }
