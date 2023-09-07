71f409b9e98549f1bdfdb35bfcbc222051009476
Gianluigi <1568018+davassi@users.noreply.github.com>
2023-09-07 17:54:35 +0200

    Adding comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use std::marker::{self, PhantomData};
 
-/// F defines the function, T the type
+// let's define a generic Filter with a generic F that defines the condition, and a generic T that defines the type
 struct FilterCondition<F, T> 
 where
     F: Fn(T) -> bool,
@@ -30,11 +30,13 @@ fn custom_filter<F : Fn(T,) -> bool, T: Copy>(v : &Vec<T>, predicate: &FilterCon
 
 fn main() -> () {
 
+    // now let's create a collection of i32 from -20 to 20 and let's filter out only the negative ones
     let predicate = FilterCondition::new(|x: i32| { x<0 });
     let v : Vec<i32> = custom_filter(&(-10..10).collect(), &predicate);
     
     println!("A i32 filtered vec: {:?}",v);
 
+    // and now let's create a collection of chars and let's filter out only the uppercase ones
     let chars : Vec<char> = vec!['a','A','b','B','c','C','d','D']; 
     let predicate = FilterCondition::new(|x: char| { x.is_uppercase() });
     let v : Vec<char> = custom_filter(&chars, &predicate);
